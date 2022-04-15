f7f59af75c50b603fa0fa8297f156cd516401013
hocheolJang <daaan0415@gmail.com>
2022-04-15 13:28:19 +0900

    폴더명 Pascal에서 camel로 변경

--- srcs/storess/defaultStores.ts
+++ srcs/storess/defaultStores.ts
@@ -1,5 +1,5 @@
 import { makeAutoObservable } from 'mobx';
-import { DefaultStoreType } from 'src/types';
+import { DefaultStoreType } from 'srcs/types';
 
 const DefaultStore: DefaultStoreType = makeAutoObservable({
   number: 10,

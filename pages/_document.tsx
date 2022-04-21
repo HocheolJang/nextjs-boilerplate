1fc36ef9f8c8edd02d40379fa64d5a5464e3e495
HocheolJang <daaan0415@gmail.com>
2022-04-21 09:05:49 +0900

    style 폴더 위치 변경, 폰트 import 위치 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,18 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css"
-          rel="stylesheet"
-          type="text/css"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
